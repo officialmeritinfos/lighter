05912cde5c527ad832820301402955d1e7682fa0
Anon Dev <michaelerastus9@gmail.com>
2025-06-05 10:31:44 +0100

    Completed

--- resources/views/user/base.blade.php
+++ resources/views/user/base.blade.php
@@ -396,6 +396,17 @@
 </script>
 @stack('js')
 
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '393f389781149d7dc80785f6180c059e119eef6f';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </body>
 </html>
